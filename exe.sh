fd6748417050557fbe898c889cf17bbbf8bdd8ad
gustavo henrique <gusta@LAPTOP-MO98D2DV.localdomain>
2023-07-07 18:53:26 -0300

    atualizando shell

--- exe.sh
+++ exe.sh
@@ -2,15 +2,21 @@
 
 exe() {
     if [ "$#" -eq 1 ]; then
-        g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out
+        clear && g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < entrada
     elif [ "$#" -eq 2 ]; then
-        g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < "$2"
+        clear && g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < "$2"
     elif [ "$#" -eq 3 ]; then
-        g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < "$2" > "$3"
+        clear && g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < "$2" > "$3"
     else
         echo "Uso incorreto: exe <arquivo.cpp> [entrada.txt] [saida.txt]"
     fi
 }
 
-# Chame a função exe() com os argumentos adequados
-exe "$@"
+# Validate the number of arguments
+if [ "$#" -gt 3 ]; then
+    echo "Erro: número de parâmetros inválido"
+    echo "Uso correto: exe <arquivo.cpp> [entrada.txt] [saida.txt]"
+else
+    # Call the exe() function with the appropriate arguments
+    exe "$@"
+fi
