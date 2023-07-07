c42278fb2513afbf73c169381c3580316d1ab796
gustavo henrique <gusta@LAPTOP-MO98D2DV.localdomain>
2023-07-07 10:23:30 -0300

    arquivos default

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// TODO: corrigir modal -> lateral.    
+// TODO: trocar ícone de lixo.
+// TODO: transformar função em service para evitar dependência ciclica.
+
+
+int main() {
+    
+}
---
+++ node.h
@@ -0,0 +1,32 @@
+#ifndef NODE_H
+#define NODE_H
+#include <string>
+using namespace std;
+
+template<typename T>
+class Node {
+private:
+    T value;  // valor 
+    Node<T> *left;  // ponteiro para filho esquerdo
+    Node<T> *right;  // ponteiro para filho direito
+public:
+    /**
+     * Construtor que recebe o valor que o nó irá armazenar e os ponteiros para os 
+     * filhos esquerdo e direito, respectivamente.
+     * */
+    Node(T val, Node<T> *l, Node<T> *r);
+    
+    /**
+    Métodos gets e sets dos atributos
+    **/
+    T getValue();
+    Node<T>* getLeft();
+    Node<T>* getRight();
+    
+    
+    void setValue(T newValue);
+    void setLeft(Node<T>* newLeft);
+    void setRight(Node<T>* newRight);
+};
+#include "node.cpp"
+#endif
