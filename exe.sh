d99c2daa7582f4ba0d288ebe8481aef4c37f1eaa
gustavo henrique <gusta@LAPTOP-MO98D2DV.localdomain>
2023-07-07 18:42:27 -0300

    ajuste .sh

--- exe.sh
+++ exe.sh
@@ -1,1 +1,16 @@
-clear && g++ main.cpp -Wall && ./a.out < entrada
+#!/bin/bash
+
+exe() {
+    if [ "$#" -eq 1 ]; then
+        g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out
+    elif [ "$#" -eq 2 ]; then
+        g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < "$2"
+    elif [ "$#" -eq 3 ]; then
+        g++ -Wall -Wextra -Werror -O2 -std=c++20 "$1" && ./a.out < "$2" > "$3"
+    else
+        echo "Uso incorreto: exe <arquivo.cpp> [entrada.txt] [saida.txt]"
+    fi
+}
+
+# Chame a função exe() com os argumentos adequados
+exe "$@"
