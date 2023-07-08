25a18de867312ce8badc4f44dbafebeff0c830a9
Estevam1to <estevamchaves@alu.ufc.br>
2023-07-08 19:06:35 -0300

    modificando a main

--- main.cpp
+++ main.cpp
@@ -32,120 +32,95 @@ int lenDiseases, lenQuestions;
 BinaryTree<int> * diseaseTree = new BinaryTree<int>(1);
 
 void getDataFromFile() {
-  ifstream file("in.txt");
+ifstream file("in.txt");
 
-  if (file.is_open()) {
-      string line;
-      getline(file, line);
+if (file.is_open()) {
+    string line;
+    getline(file, line);
 
-      lenDiseases = stoi(line.substr(0, line.find(" ")));
-      lenQuestions = stoi(line.substr(line.find(" ") + 1, line.size()));
+    lenDiseases = stoi(line.substr(0, line.find(" ")));
+    lenQuestions = stoi(line.substr(line.find(" ") + 1, line.size()));
 
-      int itDiseases = 0, itQuestions = 0;
+    int itDiseases = 0, itQuestions = 0;
 
-      if(file.is_open()) {
-        while (getline(file, line)) {
-          if (itDiseases < lenDiseases) {
+    if(file.is_open()) {
+      while (getline(file, line)) {
+        if (itDiseases < lenDiseases) {
 
-            diseases.push_back(line);
-            ++ itDiseases;
+          diseases.push_back(line);
+          ++ itDiseases;
 
-          } else if(itQuestions < lenQuestions) {
+        } else if(itQuestions < lenQuestions) {
 
-            questions.push_back(line);
-            ++ itQuestions;
+          questions.push_back(line);
+          ++ itQuestions;
 
-          } else {
-            int firstTab = line.find(TAB_CODE);
-            
-            int keyOfDesease = stoi(line.substr(0, firstTab));
-            
-            string code = line.substr(firstTab + 1, line.size());
-            string::iterator end_pos = remove(code.begin(), code.end(), TAB_CODE);
+        } else {
+          int firstTab = line.find(TAB_CODE);
           
-            code.erase(end_pos, code.end());
-            
-            diseasesCodes[code].insert(keyOfDesease);
-          }
+          int keyOfDesease = stoi(line.substr(0, firstTab));
+          
+          string code = line.substr(firstTab + 1, line.size());
+          string::iterator end_pos = remove(code.begin(), code.end(), TAB_CODE);
+        
+          code.erase(end_pos, code.end());
+          
+          diseasesCodes[code].insert(keyOfDesease);
         }
-
-        file.close();
-      } else {
-        cout << "Não foi possível abrir o arquivo informado!" << nl;
       }
+
+      file.close();
+    } else {
+      cout << "Não foi possível abrir o arquivo informado!" << nl;
     }
+  }
+}
+
+void buildDiseaseTree(){
+  Node<int> *root = new Node<int>(1, nullptr, nullptr);
+  
+  Node<int> * left   = nullptr;
+  Node<int> * right  = nullptr;
+
+  queue<Node<int>  *> nodes;
+
+  nodes.push(root);
+
+  while(!nodes.empty()) {
+    Node<int> * current = nodes.front(); 
+    nodes.pop();
+    
+    if(current->getValue() == lenQuestions)continue;
+
+    left  = new Node<int>(current->getValue() + 1, nullptr, nullptr);
+    right = new Node<int>(current->getValue() + 1, nullptr, nullptr);
+
+    current->setLeft(left);
+    current->setRight(right);
+
+    nodes.push(left);
+    nodes.push(right);
   }
   
-  void buildDiseaseTree(){
-    Node<int> *root = new Node<int>(1, nullptr, nullptr);
-    
-    Node<int> * left   = nullptr;
-    Node<int> * right  = nullptr;
+  diseaseTree->setRoot(root);
+}
 
-    queue<Node<int>  *> nodes;
+char doQuestion(Node<int> * root) {
+  cout << "Pergunta: " << questions[root->getValue() - 1] << " (S/N)" << nl;
 
-    nodes.push(root);
+  char answer;
+  cin >> answer;
 
-    while(!nodes.empty()) {
-      Node<int> * current = nodes.front(); 
-      nodes.pop();
-      
-      if(current->getValue() == lenQuestions)continue;
-
-      left  = new Node<int>(current->getValue() + 1, nullptr, nullptr);
-      right = new Node<int>(current->getValue() + 1, nullptr, nullptr);
-
-      current->setLeft(left);
-      current->setRight(right);
-
-      nodes.push(left);
-      nodes.push(right);
-    }
-    
-    diseaseTree->setRoot(root);
+  if(toupper(answer) == 'S') {
+    return '1';
+  } else {
+    return '0';
   }
+}
 
-  char doQuestion(Node<int> * root) {
-    cout << "Pergunta: " << questions[root->getValue() - 1] << " (S/N)" << nl;
+void searchDisease(Node<int> * root, string ans) {
 
-    char answer;
-    cin >> answer;
-
-    if(toupper(answer) == 'S') {
-      return '1';
-    } else {
-      return '0';
-    }
-  }
-
-  void searchDisease(Node<int> * root, string ans) {
-
-    if(root->getLeft() == nullptr && root->getRight() == nullptr) {
-      if(doQuestion(root) == '1') {
-        ans[root->getValue() - 1] = '1';
-        root = root->getLeft();
-      } else {
-        ans[root->getValue() - 1] = '0';
-        root = root->getRight();
-      }
-
-      cout << "Resposta: " << ans << nl;
-      
-      unordered_set<int> diseasesKeys = diseasesCodes[ans];
-
-      if(diseasesKeys.size()) {
-        for(int i: diseasesKeys) {
-          cout << "Doença: " << diseases[i - 1] << nl;
-        }
-
-        return;
-      }
-
-      cout << "Doença não identificada!" << nl;
-
-      return;
-    }
-
+  if(root->getLeft() == nullptr && root->getRight() == nullptr) {
     if(doQuestion(root) == '1') {
       ans[root->getValue() - 1] = '1';
       root = root->getLeft();
@@ -154,20 +129,66 @@ void getDataFromFile() {
       root = root->getRight();
     }
 
-    return searchDisease(root, ans);
+    cout << "Resposta: " << ans << nl;
+    
+    unordered_set<int> diseasesKeys = diseasesCodes[ans];
+
+    if(diseasesKeys.size()) {
+      for(int i: diseasesKeys) {
+        cout << "Doença: " << diseases[i - 1] << nl;
+      }
+
+      return;
+    }
+
+    cout << "Doença não identificada!" << nl;
+
+    return;
   }
 
-  int main() {
-    getDataFromFile();
+  if(doQuestion(root) == '1') {
+    ans[root->getValue() - 1] = '1';
+    root = root->getLeft();
+  } else {
+    ans[root->getValue() - 1] = '0';
+    root = root->getRight();
+  }
 
+  return searchDisease(root, ans);
+}
+
+int main() {
+    getDataFromFile();
     buildDiseaseTree();
 
-    string ans = "";
-    
-    for(int i = 0; i < lenQuestions; ++ i) 
-      ans += "0";
+    bool continuar = true;
 
-    searchDisease(diseaseTree->getRoot(), ans);
+    while (continuar) {
+      cout << "Pesquisar doenças?" << nl;
+      cout << "Digite 1 para continuar ou 2 para sair: ";
 
-    return 0;
-  }
+      char op;
+      cin >> op;
+
+      switch (op) {
+      case '1': {
+        string ans = "";
+
+        for (int i = 0; i < lenQuestions; ++i)
+          ans += "0";
+
+        searchDisease(diseaseTree->getRoot(), ans);
+        break;
+      }
+      case '2':
+        continuar = false;
+        break;
+      default:
+        cout << "Opção inválida. Por favor, tente novamente." << nl;
+        break;
+      }
+    }
+
+return 0;
+
+}
